01d0f5c3cdebd73c713e36c290bc4e973095d035
ArchyScript <yungdansa@gmail.com>
2021-10-05 20:48:49 +0100

    ...

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
+// import Profile from "../components/Profile/Profile.vue";
+
+const Home = () => import(/* webpackChunkName: "about" */ '../components/Home/Main.vue')
+const About = () => import(/* webpackChunkName: "about" */ '../components/About/Main.vue')
+const Profile = () => import(/* webpackChunkName: "about" */ '../components/Profile/Main.vue')
+const Media = () => import(/* webpackChunkName: "about" */ '../components/Media/Main.vue')
+
+import { authGuard } from "../auth/authGuard";
+
 
 Vue.use(VueRouter)
 
@@ -8,15 +17,24 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    // redirect: "/home",
     component: Home
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: "/profile",
+    name: "Profile",
+    component: Profile,
+    beforeEnter: authGuard
+  },
+  {
+    path: "/media",
+    name: "Media",
+    component: Media
   }
 ]
 
